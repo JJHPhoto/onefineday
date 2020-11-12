0b2279688d1084ed8cb2b4fcf1e47cf284f1d798
Joshua J. Haller <phone@joshuajhaller.com>
2020-11-11 16:10:18 -0800

    Got the colors to change in my rows based on time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,50 @@
+// Global Variables.
+let today = moment().format("MMMM Do, YYYY");
+let timeNow = moment().format("H");
+console.log(timeNow);
+let nine = $("#9").val();
+console.log(nine);
+const savedText = document.getElementsByClassName("col-10");
+console.log(savedText);
+// const rowColor = document.getElementsByClassName("hour");
+// let cycleThrough;
+
 //Current date displayed in Jumbotron
-var today = moment().format("MMMM Do,YYYY");
 $("#currentDay").text(today);
 
-//DISPLAY current time, past time & future of a normal work day.
+//Saving "textarea" to local storage
+
+//Changes the color of the row in the schedule based on time of day.
+Array.from(savedText).forEach((row) => {
+  let rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    if (timeNow === rowHour) {
+      setColor(row, "red");
+    } else if (timeNow < rowHour && timeNow > rowHour - 6) {
+      setColor(row, "#77dd77");
+    } else if (timeNow > rowHour && timeNow < rowHour + 6) {
+      setColor(row, "#d3d3d3");
+    } else {
+      setColor(row, "#ff6961");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+// $("#9").html();
+// localStorage.setItem();
+
+//Function that changes "textarea" based on past, present or future time.
+// function timeBackground() {
+//   $();
+// }
 
 //TEXT INPUT for events during the day
 //ADD text to local storage with save button.
@@ -11,3 +53,12 @@ $("#currentDay").text(today);
 //====
 //Put a console.log on button to test "click."
 //Event delegation to click any "save" button.
+
+//Tried code
+//===
+// $(document).ready(function(){
+//   var saveOnSchedule = document.getElementById("saveOnSchedule");
+//   for (var i =1; i < 11; i++) {
+//     $("saveOnSchedule")
+//   }
+// })
