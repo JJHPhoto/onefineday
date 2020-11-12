c0eb055b2a6435411a5bf31f5b17de18a13a5841
Joshua J. Haller <phone@joshuajhaller.com>
2020-11-12 10:36:23 -0800

    Updated README and cleaned up my JS.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # onefineday
 
-## 1. Initial Work
+## 1. My Workflow
 
 The first thing I did was take notes during our instructors demo of some of the material we could use in our homework assignment. After that, I tested some JS a classmate had tried to use to get his project working. He was very clear it didn't work, but it was a starting place. I then read through the README given to us by Trilology and pseudocoded my JS with what I thought would need to be done.
 
-The next thing I did was connect my HTML with the CSS style sheet provided by Trilology. This would give me a better understanding of where things were and how to connect them. Once that was done I went back and reviewed the classes that demo'd this homewore assignment.
+The next thing I did was connect my HTML with the CSS style sheet provided by Trilology. This would give me a better understanding of where things were and how to connect them. Once that was done I went back and reviewed the classes that demo'd this homework assignment. At the advice from my study group, I also went back and reviewed class activities that did similar things in code. I'd plug in that code and modify it to fit my project.
+
+After that, I also looked up a few things online that were more specific to what I wanted to do. Maybe I'm getting better at Google, but I was able to find some very good code online that was similiar enough to my work that I could modify it to work for me.
+
+At this point, I was happy to take on the most difficult part: local storage.
+
+## 2. Resources
+
+[Stack Overflow](https://stackoverflow.com/questions/59995703/trying-to-change-background-color-based-off-if-the-hour-is-in-the-past-current)
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,33 +6,31 @@ let nine = $("#9").val();
 console.log(nine);
 const scheduleText = document.getElementsByClassName("col-10");
 console.log(scheduleText);
-// const rowColor = document.getElementsByClassName("hour");
-// let cycleThrough;
 
 //Current date displayed in Jumbotron
 $("#currentDay").text(today);
 
 //Changes the color of the row in the schedule based on time of day.
 Array.from(scheduleText).forEach((row) => {
-  let rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
+  let rowSelector = row.id,
+    rowSchedule;
+  if (rowSelector) {
+    rowSchedule = parseInt(rowSelector);
   }
-  if (rowHour) {
-    if (timeNow === rowHour) {
-      setColor(row, "blue");
-    } else if (timeNow < rowHour && timeNow > rowHour - 12) {
-      setColor(row, "#77dd77");
-    } else if (timeNow > rowHour && timeNow < rowHour + 12) {
-      setColor(row, "#d3d3d3");
+  if (rowSchedule) {
+    if (timeNow === rowSchedule) {
+      changeColor(row, "blue");
+    } else if (timeNow < rowSchedule && timeNow > rowSchedule - 12) {
+      changeColor(row, "#77dd77");
+    } else if (timeNow > rowSchedule && timeNow < rowSchedule + 12) {
+      changeColor(row, "#d3d3d3");
     } else {
-      setColor(row, "#ff6961");
+      changeColor(row, "#ff6961");
     }
   }
 });
 
-function setColor(element, color) {
+function changeColor(element, color) {
   element.style.backgroundColor = color;
 }
 
@@ -41,11 +39,6 @@ function setColor(element, color) {
 // $("#9").html();
 // localStorage.setItem();
 
-//Function that changes "textarea" based on past, present or future time.
-// function timeBackground() {
-//   $();
-// }
-
 //TEXT INPUT for events during the day
 //ADD text to local storage with save button.
 
@@ -53,12 +46,3 @@ function setColor(element, color) {
 //====
 //Put a console.log on button to test "click."
 //Event delegation to click any "save" button.
-
-//Tried code
-//===
-// $(document).ready(function(){
-//   var saveOnSchedule = document.getElementById("saveOnSchedule");
-//   for (var i =1; i < 11; i++) {
-//     $("saveOnSchedule")
-//   }
-// })
